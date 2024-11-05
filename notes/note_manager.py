a4228583738c242b05452c58521d5af10c8ba9d2
SerAnatoliy <anatolii@maximustribe.com>
2024-11-05 21:19:29 +0100

    notemanager class

--- notes/note_manager.py
+++ notes/note_manager.py
@@ -1,13 +1,14 @@
 import pickle
+from utils.table_response import TableResponse
+from dataclasses import dataclass
 from pathlib import Path
 from .note import Note
 
+@dataclass
 class NoteManager:
-    def __init__(self, file_path):
-        self.file_path = Path(file_path)
+    pickle_file = 'note_book.pickle'
+    def __init__(self):
         self.notes = []
-        if self.file_path.exists():
-            self.load_from_file()
             
     def save_to_file(self):
         with open(self.file_path, "wb") as file:
@@ -20,10 +21,32 @@ class NoteManager:
         except FileNotFoundError:
             self.notes = []
 
-    def add_note(self, title, content, tags=None):
+    def add_note(self, note: Note):
+        self.notes.append(note)
 
-    def search_notes(self, query):
+    def search_notes(self, query:str) -> Note:
+        for note in self.notes:
+            if note.title == query:
+                return note
+        return None
 
-    def delete_note(self, title):
+    def delete_note(self, title:str) -> Note:
+        for note in self.notes:
+            if note.title == title:
+                self.notes.remove(note)
+                return note
+        return None
 
-    def edit_note(self, title, new_content):
+    def edit_note(self, title:str, new_content:str) -> Note:
+        note = self.search_notes(title)
+        if note:
+            note.change(new_content)
+        return note
+    
+    def list_notes(self) -> list[Note]:
+        return self.notes
+    
+    def __repr__(self):
+        body = [[note.title, note.value, ', '.join(note.tags)] for note in self.notes] if self.notes else [
+            ["", "", ""]]
+        return repr(TableResponse(headers=["Title", "Content", "Tags"], body=body))
